3933694ae0421efa87b6500ae91be7ce40888953
Ippei Mukaida <ippei-mukaida@cybozu.co.jp>
2025-07-29 11:29:05 +0900

    Fix annotation format

--- feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadViewModel.kt
+++ feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadViewModel.kt
@@ -19,22 +19,20 @@ interface ThreadViewModelFactory {
 }
 
 @HiltViewModel(assistedFactory = ThreadViewModelFactory::class)
-class ThreadViewModel
-    @AssistedInject
-    constructor(
-        @Assisted private val threadId: String,
-        private val repository: SpaceRepository,
-    ) : ViewModel() {
-        private val _messages = MutableStateFlow<List<KintoneMessage>>(emptyList())
-        val messages: StateFlow<List<KintoneMessage>> = _messages.asStateFlow()
+class ThreadViewModel @AssistedInject constructor(
+    @Assisted private val threadId: String,
+    private val repository: SpaceRepository,
+) : ViewModel() {
+    private val _messages = MutableStateFlow<List<KintoneMessage>>(emptyList())
+    val messages: StateFlow<List<KintoneMessage>> = _messages.asStateFlow()
 
-        init {
-            loadMessages()
-        }
+    init {
+        loadMessages()
+    }
 
-        private fun loadMessages() {
-            viewModelScope.launch {
-                _messages.value = repository.getMessagesForThread(threadId)
-            }
+    private fun loadMessages() {
+        viewModelScope.launch {
+            _messages.value = repository.getMessagesForThread(threadId)
         }
     }
+}
