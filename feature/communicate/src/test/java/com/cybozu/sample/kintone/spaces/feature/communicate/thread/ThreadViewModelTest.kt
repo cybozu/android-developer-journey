ebcbe14b9c65e9d4f7dd1f6bd7766c76deb94c6e
Omj-8 <Omj-8@users.noreply.github.com>
2025-08-20 10:09:16 +0900

    back

--- feature/communicate/src/test/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadViewModelTest.kt
+++ feature/communicate/src/test/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadViewModelTest.kt
@@ -47,14 +47,15 @@ class ThreadViewModelTest {
                 (loadingState is ThreadUiState.Loading) shouldBe true
 
                 val loadedState = awaitItem()
-                (loadedState as ThreadUiState.Success)
-                loadedState.threadMessage.size shouldBe 2
-                loadedState.threadMessage[0].id shouldBe "msg-1"
-                loadedState.threadMessage[0].body shouldBe "thread-1"
-                loadedState.threadMessage[0].creator shouldBe Creator(name = "name1")
-                loadedState.threadMessage[1].id shouldBe "msg-2"
-                loadedState.threadMessage[1].body shouldBe "thread-2"
-                loadedState.threadMessage[1].creator shouldBe Creator(name = "name2")
+                (loadedState is ThreadUiState.Success) shouldBe true
+                val successState = loadedState as ThreadUiState.Success
+                successState.threadMessage.size shouldBe 2
+                successState.threadMessage[0].id shouldBe "msg-1"
+                successState.threadMessage[0].body shouldBe "thread-1"
+                successState.threadMessage[0].creator shouldBe Creator(name = "name1")
+                successState.threadMessage[1].id shouldBe "msg-2"
+                successState.threadMessage[1].body shouldBe "thread-2"
+                successState.threadMessage[1].creator shouldBe Creator(name = "name2")
             }
         }
 }
