9da6ef320446c56563202e8f27a0fb10cdad8d4e
Omj-8 <Omj-8@users.noreply.github.com>
2025-08-20 09:22:58 +0900

    ThreadScreen-add-InitialState

--- feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadScreen.kt
+++ feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadScreen.kt
@@ -83,15 +83,6 @@ fun ThreadContent(
     ) { innerPadding ->
         when (uiState) {
             is ThreadUiState.Initial -> {
-                Box(
-                    modifier =
-                        Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
             }
             is ThreadUiState.Loading -> {
                 Box(
