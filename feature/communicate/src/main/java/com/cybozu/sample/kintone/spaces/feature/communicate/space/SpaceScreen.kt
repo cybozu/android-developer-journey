62b7e075f56c73f80d4cb76a0f37a90ab5d6b21e
GataGataGAta <gata8810.1127@gmail.com>
2026-09-07 17:14:57 +0900

    PRの練習用commit

--- feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/space/SpaceScreen.kt
+++ feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/space/SpaceScreen.kt
@@ -52,7 +52,7 @@ fun SpaceContent(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Spaces") })
+            TopAppBar(title = { Text("hogehoge") })
         }
     ) { innerPadding ->
         if (uiState.isLoading) {
