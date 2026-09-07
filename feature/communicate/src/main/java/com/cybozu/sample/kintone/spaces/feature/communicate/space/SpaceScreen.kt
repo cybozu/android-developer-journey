bfe2aee5cd9aeeadb728efbdd8c67734d305e10d
nekomoti22 <nekomotinkmt22@gmail.com>
2026-09-07 17:23:35 +0900

    try commit 2

--- feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/space/SpaceScreen.kt
+++ feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/space/SpaceScreen.kt
@@ -52,7 +52,7 @@ fun SpaceContent(
 ) {
     Scaffold(
         topBar = {
-            TopAppBar(title = { Text("Spaces") })
+            TopAppBar(title = { Text("タイムラインです") })
         }
     ) { innerPadding ->
         if (uiState.isLoading) {
