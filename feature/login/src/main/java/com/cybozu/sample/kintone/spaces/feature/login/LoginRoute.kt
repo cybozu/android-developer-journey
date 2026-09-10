061b25d9364934d682159fc368d206e1ce4ca9a3
GataGataGAta <gata8810.1127@gmail.com>
2026-09-10 15:56:47 +0900

    ログイン画面のナビゲーションを実装

---
+++ feature/login/src/main/java/com/cybozu/sample/kintone/spaces/feature/login/LoginRoute.kt
@@ -0,0 +1,8 @@
+package com.cybozu.sample.kintone.spaces.feature.login
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data object LoginRoute : NavKey
