531945990765a367c5e375b98407873c789567ec
makino-yuto <yuuto01222005@gmail.com>
2026-09-10 17:02:13 +0900

    task4 imePadding()の追加

--- feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadScreen.kt
+++ feature/communicate/src/main/java/com/cybozu/sample/kintone/spaces/feature/communicate/thread/ThreadScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -112,6 +114,8 @@ fun ThreadContent(
                 modifier =
                     Modifier
                         .fillMaxWidth()
+                        .imePadding()
+                        .navigationBarsPadding()
                         .padding(8.dp),
                 verticalAlignment = Alignment.CenterVertically
             ) {
